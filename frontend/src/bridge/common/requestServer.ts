1c8aaa09724d976d72aa87bbba312bc777a956b5
nabsba <ns-dev@outlook.com>
2021-11-27 16:45:34 +0000

    Feat(requestServer.ts): add the own function call for api

--- frontend/src/bridge/common/requestServer.ts
+++ frontend/src/bridge/common/requestServer.ts
@@ -5,7 +5,7 @@ import { Result } from '../../Common/type/type';
 const resultTemplate: Result = {
 	state: false,
 	data: null,
-	error: '',
+	errorCodeServer: '',
 	serverError: false,
 	errorMessage: '',
 	errorCodeSql: '',
@@ -28,6 +28,36 @@ __proto__: Object
 } */
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+const serverGetApi = async (
+	url: string,
+	time?: number | null,
+	source?: any,
+	params?: any,
+): Promise<Result> => {
+	const result: Result = { ...resultTemplate };
+	let query: any = {};
+	try {
+		query = await axios.get(url, {
+			timeout: time ? time : 15000,
+			cancelToken: source ? source.token : null,
+			params,
+		});
+	} catch (error) {
+		console.log(
+			'file: bridge/requestServer, method: serverGet, error: ',
+			error,
+		);
+		result.serverError = true;
+		result.errorMessage =
+			typeof error === 'string' ? error : JSON.stringify(error);
+	} finally {
+		result.state = Boolean(query.data);
+		result.data = query.data;
+		return result;
+	}
+};
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 const serverGet = async (
 	url: string,
 	time?: number | null,
@@ -46,8 +76,9 @@ const serverGet = async (
 			'file: bridge/requestServer, method: serverGet, error: ',
 			error,
 		);
-		result.error = '400';
 		result.serverError = true;
+		result.errorMessage =
+			typeof error === 'string' ? error : JSON.stringify(error);
 	} finally {
 		result = result.data ? result.data : result;
 		return result;
@@ -69,7 +100,7 @@ const serverPost = async (
 			error,
 		);
 		result.serverError = true;
-		result.error = '500';
+		result.errorCodeServer = '500';
 	} finally {
 		// times = new Date().toLocaleTimeString();
 
@@ -78,4 +109,4 @@ const serverPost = async (
 	}
 };
 
-export { serverGet, serverPost, resultTemplate };
+export { serverGetApi, serverPost, serverGet, resultTemplate };
