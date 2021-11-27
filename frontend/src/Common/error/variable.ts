b416e09f870baa038b4a4a73d07421d959b8dc78
nabsba <ns-dev@outlook.com>
2021-11-27 16:57:48 +0000

    Feat(variable.ts): init constant ERROR_MESSAGE

---
+++ frontend/src/Common/error/variable.ts
@@ -0,0 +1,8 @@
+const ERROR_MESSAGE = {
+	DEFAULT: 'An error has occured',
+	_400: 'The request cannot be fulfilled due to bad syntax',
+	_404: 'The requested page could not be found but may be available again in the future',
+	_500: 'Internal Server Error',
+};
+
+export default ERROR_MESSAGE;
