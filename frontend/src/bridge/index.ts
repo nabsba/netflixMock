53ff66ec8b810359d5ae9567a5b65ca606534bc1
nabsba <ns-dev@outlook.com>
2021-11-28 14:17:47 +0000

    Feat(bridge/index.ts): add index.ts file

---
+++ frontend/src/bridge/index.ts
@@ -0,0 +1,9 @@
+import {
+	serverGetApi,
+	serverPost,
+	serverGet,
+	resultTemplate,
+} from './common/requestServer';
+import URL_ADDRESSES from './url';
+
+export { URL_ADDRESSES, serverGetApi, serverPost, serverGet, resultTemplate };
