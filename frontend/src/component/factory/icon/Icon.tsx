a69e68cfc594bca946a5abb68e7c093829830544
nabsba <ns-dev@outlook.com>
2021-11-29 11:13:36 +0000

    Feat(icon): add ArrowDown + NotificationIcon + ProfileIcon + Search

--- frontend/src/component/factory/icon/Icon.tsx
+++ frontend/src/component/factory/icon/Icon.tsx
@@ -1,5 +1,10 @@
 import React, { ReactElement } from 'react';
-import { Search } from '../../tree/atom/icon';
+import {
+	ArrowDown,
+	NotificationIcon,
+	ProfileIcon,
+	Search,
+} from '../../tree/atom/icon';
 import IconWrapper from '../../tree/atom/icon/IconWrapper';
 
 // https://react-icons.github.io/react-icons/icons?name=fa
@@ -8,6 +13,13 @@ const getIcon = (indice: string): ReactElement => {
 	switch (indice) {
 		case 'Search':
 			return <IconWrapper Icon={<Search />} />;
+		case 'ArrowDown':
+			return <IconWrapper Icon={<ArrowDown />} />;
+		case 'NotificationIcon':
+			return <IconWrapper Icon={<NotificationIcon />} />;
+		case 'ProfileIcon':
+			return <ProfileIcon />;
+
 		default:
 			return <span> {indice}</span>;
 	}
--- frontend/src/component/tree/atom/icon/index.ts
+++ frontend/src/component/tree/atom/icon/index.ts
@@ -1,2 +1,6 @@
 import { AiOutlineSearch as Search } from 'react-icons/ai';
-export { Search };
+import { MdOutlineArrowDropDown as ArrowDown } from 'react-icons/md';
+import { IoMdNotifications as NotificationIcon } from 'react-icons/io';
+import ProfileIcon from './Profile';
+
+export { Search, ArrowDown, NotificationIcon, ProfileIcon };
