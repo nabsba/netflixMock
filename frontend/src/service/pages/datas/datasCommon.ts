18e3c417f787a297c75cc117305b06c93127b0b8
nabsba <ns-dev@outlook.com>
2021-11-29 17:22:49 +0000

    Feat(page/datasCommon): add shared between pages

---
+++ frontend/src/service/pages/datas/datasCommon.ts
@@ -0,0 +1,20 @@
+const header = {
+	navigation: {
+		listNavigation: ['Home', 'TV Show', 'Movies', 'Latest', 'My list'],
+	},
+	movieBackground: {
+		background: {
+			src: 'https://occ-0-475-1167.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABQHPJ2jrCTdo1LCvqdDYLYeX1YDLNpvS8oe8OribopJ9em2i-HKFaidmFmjcd0MgAjqNUI5sZSgoSlC1F0UCDtL39gN2.webp?r=f3a',
+			alt: 'Arcane',
+		},
+		description: {
+			src: 'https://occ-0-475-1167.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABcL0sOLj3ZviPqIEs-lqt_7s_D5utZHftF0QKqLnrWeGgh3Cd-taT4S4cFjeHb62iAumeN_JaizOXf7NO5YZfmt6GPo5b8Sqxb5sD56EE0D2p89n2RGa5B7akZqwbHQ80fQA_7zEGXUzBdSKJpdeD77mumF2My12dWxfzOPo3Qk6SA.webp?r=183',
+			alt: 'Arcane',
+		},
+	},
+	h2: 'Regardez la saison 1 maintenant',
+	paragraph:
+		'Set in utopian Piltover and the oppressed underground of Zaun, the story follows the origins of two iconic League champions-and the power that will tear them apart.',
+};
+
+export { header };
