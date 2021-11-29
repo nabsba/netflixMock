b8eeafb89532565cddd6dcc320f16549ed396eb9
nabsba <ns-dev@outlook.com>
2021-11-29 17:23:34 +0000

    Feat(pages/reducers): add data managment to retrieve data for pages

--- frontend/src/service/pages/dataManagment/reducer.ts
+++ frontend/src/service/pages/dataManagment/reducer.ts
@@ -1,63 +1,23 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import {
-	resultTemplate,
-	serverGet,
-} from '../../../bridge/common/requestServer';
-import URL_ADDRESSES from '../../../bridge/url';
-import { Result } from '../../../Common/type/type';
+import { createSlice } from '@reduxjs/toolkit';
 import * as dataBackup from '../datas/backup/data.json';
+import { header } from '../datas/datasCommon';
 
 // Those which are imported from home are those who the admin cannot update from his pannel.
 const initialState = {
-	pending: true,
-	state: false,
-	data: {},
-	error: '',
-	errorServer: '',
+	home: {
+		header: header,
+	},
 };
-
-export const fetchDataPages = createAsyncThunk('dataPages', async () => {
-	let result: Result = { ...resultTemplate };
-	try {
-		result = await serverGet(URL_ADDRESSES.default, null);
-		if (!result.state) {
-			result.state = true;
-			result.data = JSON.parse(JSON.stringify(dataBackup));
-		}
-		return result;
-	} catch (error) {
-		console.log(
-			'*** file: redux/midleware, method: fetchDataPages, error: ',
-			error,
-		);
-	}
-});
 
 const data = createSlice({
 	name: 'dataPages',
 	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		builder.addCase(
-			fetchDataPages.fulfilled,
-			/*eslint-disable-next-line  @typescript-eslint/no-explicit-any*/
-			(state, action: { payload: any }) => {
-				// Add user to the state array
-				state.state = true;
-				state.data = action.payload;
-			},
-		);
-		builder.addCase(fetchDataPages.rejected, (state) => {
-			state.state = false;
-			state.pending = false;
-		});
-		builder.addCase(fetchDataPages.pending, (state) => {
-			state.state = false;
-			state.pending = true;
-		});
+	reducers: {
+		getDatasPages: (state) => state,
 	},
 });
 const dataPages = data.reducer;
+const { getDatasPages } = data.actions;
 
 export default dataPages;
-export { dataBackup };
+export { dataBackup, getDatasPages };
