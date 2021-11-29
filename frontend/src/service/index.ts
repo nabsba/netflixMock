29f972a0267f07510a36a7dfade49ad4d2a117b0
nabsba <ns-dev@outlook.com>
2021-11-29 17:34:59 +0000

    Feat(service/index.ts): update file"

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -1,7 +1,18 @@
+import { getBrowserInfo, updateInfoDevice } from './browserInfo/reducer';
+import { TBrowserInfo } from './browserInfo/type';
 import reducers from './Common/reducer';
 import { TReducers } from './Common/type';
 import { fetchDataNetflix } from './netflix/dataManagment/reducer';
+import { dataBackup, getDatasPages } from './pages/dataManagment/reducer';
 import { DATA_TYPE_SERVICE_WORKER } from './wpa/serviceWorker/data';
 
-export { DATA_TYPE_SERVICE_WORKER, reducers, fetchDataNetflix };
-export type { TReducers };
+export {
+	DATA_TYPE_SERVICE_WORKER,
+	reducers,
+	fetchDataNetflix,
+	updateInfoDevice,
+	getBrowserInfo,
+	dataBackup,
+	getDatasPages,
+};
+export type { TReducers, TBrowserInfo };
