235ccbcf4e7be56fc8afb36f892dbbbfb243f1aa
nabsba <ns-dev@outlook.com>
2021-11-29 11:16:50 +0000

    Feat(atom/index): update file

--- frontend/src/component/tree/atom/index.ts
+++ frontend/src/component/tree/atom/index.ts
@@ -1,1 +1,17 @@
-export {};
+import IconWrapper from './icon/IconWrapper';
+import ImageAsComponent from './image/Image';
+import NavLinkAsComponent from './link/NavLink';
+import Paragraph from './paragraph/Paragraph';
+import Span from './span/Span';
+import H1 from './title/h1/H1';
+import H2 from './title/h2/H2';
+
+export {
+	NavLinkAsComponent,
+	H1,
+	H2,
+	IconWrapper,
+	ImageAsComponent,
+	Span,
+	Paragraph,
+};
