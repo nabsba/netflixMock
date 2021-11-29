74847228e56cf627998de373b61c3adb0c3941cc
nabsba <ns-dev@outlook.com>
2021-11-29 17:32:23 +0000

    Refactor(service/index.ts): update file

--- frontend/src/component/tree/atom/icon/index.ts
+++ frontend/src/component/tree/atom/icon/index.ts
@@ -2,5 +2,14 @@ import { AiOutlineSearch as Search } from 'react-icons/ai';
 import { MdOutlineArrowDropDown as ArrowDown } from 'react-icons/md';
 import { IoMdNotifications as NotificationIcon } from 'react-icons/io';
 import ProfileIcon from './Profile';
+import { GrCircleAlert as Indication } from 'react-icons/gr';
+import { IoTriangleSharp as Triangle } from 'react-icons/io5';
 
-export { Search, ArrowDown, NotificationIcon, ProfileIcon };
+export {
+	Search,
+	ArrowDown,
+	NotificationIcon,
+	ProfileIcon,
+	Indication,
+	Triangle,
+};
