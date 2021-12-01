93c294bb080208f27b996ff55e5c30e1f18bbb85
nabsba <ns-dev@outlook.com>
2021-12-01 16:11:56 +0000

    Feat(molecule/Hd.tsx): add component

---
+++ frontend/src/component/tree/molecule/hd/Hd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Span } from '../../atom';
+import './style.css';
+
+const Hd: React.FC = () => {
+	return (
+		<div className="hd">
+			<Span data={'hd'} />
+		</div>
+	);
+};
+export default Hd;
