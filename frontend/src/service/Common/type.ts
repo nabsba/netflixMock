0e852472e847d5c7f79a79ca863d1403a4b4298b
nabsba <ns-dev@outlook.com>
2021-12-02 13:37:30 +0000

    Refactor(common/type.ts): refactor dataNetFlix

--- frontend/src/service/Common/type.ts
+++ frontend/src/service/Common/type.ts
@@ -1,17 +1,12 @@
 import { TBrowserInfo } from '..';
 import { THeader } from '../../component/tree/organism/header/type';
+import { TDataNetflix } from '../netflix/type';
 
 export type TReducers = {
 	dataPages: {
 		home: { header: THeader };
 		course: any;
 	};
-	dataNetflix: {
-		pending: boolean;
-		state: boolean;
-		data: any;
-		error: '';
-		errorServer: '';
-	};
+	dataNetflix: TDataNetflix;
 	browserInfo: TBrowserInfo;
 };
