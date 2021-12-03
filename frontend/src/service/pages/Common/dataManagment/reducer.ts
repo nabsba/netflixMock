d2e62b395fbc0a435669b60da54ee84c30b4b72a
nabsba <ns-dev@outlook.com>
2021-12-03 22:57:51 +0000

    Feat(Common/dataManagment): update reducer, type, data.json and datasCommon

---
+++ frontend/src/service/pages/Common/dataManagment/reducer.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import * as dataBackup from '../datas/backup/data.json';
+import { header } from '../datas/datasCommon';
+import { arcturus } from '../../home/data';
+
+// Those which are imported from home are those who the admin cannot update from his pannel.
+const initialState = {
+	home: {
+		header,
+		arcturus,
+	},
+};
+
+const data = createSlice({
+	name: 'dataPages',
+	initialState,
+	reducers: {
+		getDatasPages: (state) => state,
+		initGroupArticleWithNetflixData: (state, action) => {
+			const data = action.payload;
+			state.home.arcturus.groupArticleOne.data = data;
+		},
+		polulateDataPagesWithApiData: (state, action) => {
+			console.log('action');
+		},
+	},
+});
+const dataPages = data.reducer;
+const {
+	getDatasPages,
+	polulateDataPagesWithApiData,
+	initGroupArticleWithNetflixData,
+} = data.actions;
+
+export default dataPages;
+export { dataBackup, getDatasPages, initGroupArticleWithNetflixData };
---
+++ frontend/src/service/pages/Common/dataManagment/type.ts
@@ -0,0 +1,9 @@
+import { THeader } from '../../../../component/tree/template/header/type';
+import TArcturus from '../../../../component/tree/template/arcturus/type';
+
+type TDataPages = {
+	home: { header: THeader; arcturus: TArcturus };
+	// otherPage
+};
+
+export default TDataPages;
