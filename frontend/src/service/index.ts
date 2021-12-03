6af1e3fb9b54f7f5b799d20050649d2f95ccd5cb
nabsba <ns-dev@outlook.com>
2021-12-03 23:06:34 +0000

    Feat(service/index.ts): update imports

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -3,7 +3,14 @@ import { TBrowserInfo } from './browserInfo/type';
 import reducers from './Common/reducer';
 import { TReducers } from './Common/type';
 import { fetchDataNetflix } from './netflix/dataManagment/reducer';
-import { dataBackup, getDatasPages } from './pages/dataManagment/reducer';
+import { getVideoUrl } from './netflix/factory/functions';
+import {
+	dataBackup,
+	getDatasPages,
+	initGroupArticleWithNetflixData,
+} from './pages/Common/dataManagment/reducer';
+import TDataPages from './pages/Common/dataManagment/type';
+
 import { DATA_TYPE_SERVICE_WORKER } from './wpa/serviceWorker/data';
 
 export {
@@ -14,5 +21,7 @@ export {
 	getBrowserInfo,
 	dataBackup,
 	getDatasPages,
+	initGroupArticleWithNetflixData,
+	getVideoUrl,
 };
-export type { TReducers, TBrowserInfo };
+export type { TReducers, TBrowserInfo, TDataPages };
