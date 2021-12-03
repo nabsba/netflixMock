0bad35bf40aeb94c665e0cdabe7d78e6d40a7d5d
nabsba <ns-dev@outlook.com>
2021-12-03 23:03:55 +0000

    Feat(molecule/index.ts): update imports

--- frontend/src/component/tree/molecule/index.ts
+++ frontend/src/component/tree/molecule/index.ts
@@ -1,8 +1,18 @@
 import ArticleOne from './article/one/ArticleOne';
 import TArticleOne from './article/one/type';
+import TArticleTwo from './article/two/type';
 import ButtonRectangle from './button/rectangle/ButtonRectangle';
+import TGroupIcon from './groupIcon/type';
 import Hd from './hd/Hd';
+import TMinorInformationVideo from './minorInformationVideo/type';
+import TVideoPlayer from './video/type';
 import VideoPlayer from './video/VideoPlayer';
 
 export { ArticleOne, Hd, ButtonRectangle, VideoPlayer };
-export type { TArticleOne };
+export type {
+	TArticleOne,
+	TGroupIcon,
+	TMinorInformationVideo,
+	TVideoPlayer,
+	TArticleTwo,
+};
