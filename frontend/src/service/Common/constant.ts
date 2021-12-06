79bdb187c03d753ccc73af65e0ee559359f68879
nabsba <ns-dev@outlook.com>
2021-12-06 23:23:59 +0000

    Feat(Common/constant.ts): add constants files

---
+++ frontend/src/service/Common/constant.ts
@@ -0,0 +1,8 @@
+import { TInfosPage } from './type';
+
+const APIS = ['netflix'];
+const INFOS_PAGE_DEFAULT: TInfosPage = {
+	INDICE_TO_TRIGGER_NEW_PAGE: 5,
+	NUMBER_OF_ITEMS_IN_PAGE: 20,
+};
+export { APIS, INFOS_PAGE_DEFAULT };
