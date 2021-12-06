136d5629ea27b2799d543bd0e4b4b8de0308fbdf
nabsba <ns-dev@outlook.com>
2021-12-06 23:45:43 +0000

    Refactor(GroupArticleOne.tsx): split functions and style

--- frontend/src/component/tree/organism/groupArticle/one/GroupArticleOne.tsx
+++ frontend/src/component/tree/organism/groupArticle/one/GroupArticleOne.tsx
@@ -1,79 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { URL_ADDRESSES } from '../../../../../bridge';
 import {
-	TNetflixListMoviesReturned,
-	TNetflixMovie,
-} from '../../../../../service/netflix/type';
-import { H1 } from '../../../atom';
+	APIS,
+	completeArticleTwoWithDataReceived,
+} from '../../../../../service';
+import { H3 } from '../../../atom';
 import { TArticleTwo } from '../../../molecule';
 import ArticleTwo from '../../../molecule/article/two/ArticleTwo';
 import SliderTwo from '../../../molecule/slide/sliderTwo/SliderTwo';
 import './style.css';
-import TGroupArticleOne from './type';
+import TGroupArticleOne, { TListArticle } from './type';
 
 type Props = {
 	data: TGroupArticleOne;
 };
 
-type TListArticle = {
-	title: string;
-	allArticleMovie: TArticleTwo[];
-};
 const GroupArticleOne: React.FC<Props> = ({ data: { articleTwo, data } }) => {
-	// const getImage = () => {
 	const [listArticleTwo, setListArticleTwo] = useState<any[]>([]);
-	const APIS = ['netflix'];
-	{
-		/*todo: make swtich statement for neutral */
-	}
 	useEffect(() => {
-		{
-			/*todo: create props factory to get this*/
-		}
-		if (data.state) {
-			data.data.map((listMovieReturned: TNetflixListMoviesReturned) => {
-				const list = listMovieReturned.result.data.results;
-				const title = listMovieReturned.title;
-				const articleTwoCompletedWithDataFromAPI: TListArticle = {
-					title,
-					allArticleMovie: list.map((movie: TNetflixMovie) => {
-						return {
-							...articleTwo,
-							minorInformationVideo: {
-								number: movie.popularity,
-								time: movie.release_date,
-							},
-							videoPlayer: {
-								url: '',
-								id: movie.id,
-								typeConfiguration: 'youtube',
-								name: movie.original_title,
-								date: movie.release_date,
-								type: 'netflix',
-								isVolumeUp: 1,
-							},
-							imagePresentation: {
-								src: URL_ADDRESSES.api.netflix.image(movie.poster_path),
-								alt: movie.original_title,
-							},
-						};
-					}),
-				};
-				setListArticleTwo((prevState) => [
-					...prevState,
-					articleTwoCompletedWithDataFromAPI,
-				]);
-			});
+		if (data && data.state && articleTwo.type) {
+			const listArticleTwo = completeArticleTwoWithDataReceived(
+				articleTwo.type,
+				data,
+				articleTwo,
+			);
+			setListArticleTwo(listArticleTwo);
 		}
 	}, [data, articleTwo]);
 
 	const displayArticleMovies = (listArticle: TArticleTwo[]) => {
 		/*todo: essaye d'activer le scroll lorsque une image de film apparait en gros (hover). et desactive la quand tu es sorti. Ca permettra peut etre de conserver le scale et d'avoir acces au scroll */
 		const articles: React.ReactNode[] = [];
+		let path = '';
 		listArticle.map((article: TArticleTwo, index: number) => {
+			path = article.path ? article.path : '';
 			articles.push(
 				<ArticleTwo
-					key={APIS.includes(article.videoPlayer.type) ? index : index}
+					key={
+						APIS.includes(article.videoPlayer.type)
+							? article.videoPlayer.type + index
+							: index
+					}
 					data={article}
 				/>,
 			);
@@ -86,6 +52,9 @@ const GroupArticleOne: React.FC<Props> = ({ data: { articleTwo, data } }) => {
 						method: 'responsive',
 						animation: false,
 					},
+					// todo: remove hardcoding
+					type: 'netflix',
+					path: path,
 				}}
 			/>
 		);
@@ -93,13 +62,12 @@ const GroupArticleOne: React.FC<Props> = ({ data: { articleTwo, data } }) => {
 	const displayListWishes = () =>
 		listArticleTwo.map((listArticle: TListArticle) => (
 			<div className="group_article_one_sub_main" key={listArticle.title}>
-				<H1 title={listArticle.title} />
-				<div className="test_nabil">
+				<H3 title={listArticle.title} />
+				<div className="group_article_one_sub_main_articles_wrapper">
 					{displayArticleMovies(listArticle.allArticleMovie)}
 				</div>
 			</div>
 		));
-
 	return <div className="group_article_one">{displayListWishes()}</div>;
 };
 export default GroupArticleOne;
--- frontend/src/component/tree/organism/groupArticle/one/type.ts
+++ frontend/src/component/tree/organism/groupArticle/one/type.ts
@@ -1,7 +1,13 @@
+import { Result } from './../../../../../Common/type/type';
 import { TArticleTwo } from '../../../molecule';
 
 type TGroupArticleOne = {
 	articleTwo: TArticleTwo;
-	data: any;
+	data?: Result;
+};
+
+export type TListArticle = {
+	title: string;
+	allArticleMovie: TArticleTwo[];
 };
 export default TGroupArticleOne;
