1c518f444c180eaaf7b6d5dce1ff51f47f40f4a7
nabsba <ns-dev@outlook.com>
2021-12-07 12:43:49 +0000

    Feat(Footer.tsx): display content

--- frontend/src/component/tree/template/footer/Footer.tsx
+++ frontend/src/component/tree/template/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Paragraph, Span } from '../../atom';
 import './style.css';
 import TFooter from './type';
 
@@ -7,6 +8,25 @@ type Props = {
 };
 
 const Footer: React.FC<Props> = ({ data: { top, bottom } }) => {
-	return <div id="footer">hello footer</div>;
+	const TopHTML = () =>
+		top.list.map((list: string[], index: number) => (
+			<div key={index} className="foot_top_sub_wrapper">
+				<div key={index}>
+					{list.map((menu: string) => (
+						<Span key={menu} data={menu} />
+					))}
+				</div>
+			</div>
+		));
+
+	return (
+		<div id="footer">
+			<div className="footer_top">{(() => TopHTML())()}</div>
+			<div className="footer_bottom">
+				<Span data={bottom.span} />
+				<Paragraph data={bottom.paragraph} />
+			</div>
+		</div>
+	);
 };
 export default Footer;
