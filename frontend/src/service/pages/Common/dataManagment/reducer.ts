f03db6221ed9b1e04b923a2b81249aee8a234591
nabsba <ns-dev@outlook.com>
2021-12-08 11:33:40 +0000

    Feat(dataManagment): update folder

--- frontend/src/service/pages/Common/dataManagment/reducer.ts
+++ frontend/src/service/pages/Common/dataManagment/reducer.ts
@@ -2,6 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import * as dataBackup from '../datas/backup/data.json';
 import { header, footer } from '../datas/datasCommon';
 import { arcturus } from '../../home/data';
+import { TNetflixMovie } from '../../../netflix/type';
+import { URL_ADDRESSES } from '../../../../bridge';
+import { IMAGE_SIZE, randomIntFromInterval, VIDEO_PLAYER } from '../../..';
 
 // Those which are imported from home are those who the admin cannot update from his pannel.
 const initialState = {
@@ -18,8 +21,45 @@ const data = createSlice({
 	reducers: {
 		getDatasPages: (state) => state,
 		initGroupArticleWithNetflixData: (state, action) => {
-			const data = action.payload;
-			state.home.arcturus.groupArticleOne.data = data;
+			const { payload } = action;
+			state.home.arcturus.groupArticleOne.data = payload.data;
+			if (payload.data && payload.data.length > 0) {
+				const randomList: number = Math.round(
+					randomIntFromInterval(0, payload.data.length - 1),
+				);
+				const randomMovie = Math.round(
+					randomIntFromInterval(
+						0,
+						payload.data[randomList].result.data.results.length - 1,
+					),
+				);
+				//todo: check a random index in results not only 0. like this when you refresh it changes.
+				const movie: TNetflixMovie =
+					payload.data[randomList].result.data.results[randomMovie];
+				state.home.header = {
+					...state.home.header,
+					imageBackground: {
+						src: URL_ADDRESSES.api.netflix.image(
+							movie.backdrop_path,
+							IMAGE_SIZE.original,
+						),
+						alt: movie.title ? movie.title : movie.name,
+					},
+					articleOne: {
+						h2: { title: movie.title ? movie.title : movie.name },
+						paragraph: movie.overview,
+					},
+					videoPlayer: {
+						url: '',
+						name: movie.title,
+						typeConfiguration: VIDEO_PLAYER.typeConfiguration,
+						date: movie.release_date,
+						type: VIDEO_PLAYER.type,
+						isVolumeUp: 1,
+						id: movie.id,
+					},
+				};
+			}
 		},
 		polulateDataPagesWithApiData: (state, action) => {
 			console.log('action');
@@ -27,11 +67,7 @@ const data = createSlice({
 	},
 });
 const dataPages = data.reducer;
-const {
-	getDatasPages,
-	polulateDataPagesWithApiData,
-	initGroupArticleWithNetflixData,
-} = data.actions;
+const { getDatasPages, initGroupArticleWithNetflixData } = data.actions;
 
 export default dataPages;
 export { dataBackup, getDatasPages, initGroupArticleWithNetflixData };
--- frontend/src/service/pages/Common/datas/datasCommon.ts
+++ frontend/src/service/pages/Common/datas/datasCommon.ts
@@ -18,12 +18,8 @@ const header: THeader = {
 		},
 	},
 	imageBackground: {
-		src: 'https://occ-0-475-1167.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABQHPJ2jrCTdo1LCvqdDYLYeX1YDLNpvS8oe8OribopJ9em2i-HKFaidmFmjcd0MgAjqNUI5sZSgoSlC1F0UCDtL39gN2.webp?r=f3a',
-		alt: 'Arcane',
-	},
-	imageDescription: {
-		src: 'https://occ-0-475-1167.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABcL0sOLj3ZviPqIEs-lqt_7s_D5utZHftF0QKqLnrWeGgh3Cd-taT4S4cFjeHb62iAumeN_JaizOXf7NO5YZfmt6GPo5b8Sqxb5sD56EE0D2p89n2RGa5B7akZqwbHQ80fQA_7zEGXUzBdSKJpdeD77mumF2My12dWxfzOPo3Qk6SA.webp?r=183',
-		alt: 'Arcane',
+		src: '',
+		alt: '',
 	},
 	articleOne: {
 		h2: { title: 'Regardez la saison 1 maintenant' },
@@ -39,6 +35,14 @@ const header: THeader = {
 			text: 'more',
 			componentPassed: getIcon('Indication'),
 		},
+	},
+	videoPlayer: {
+		url: '',
+		name: '',
+		typeConfiguration: 'youtube',
+		date: '',
+		type: 'netflix',
+		isVolumeUp: 1,
 	},
 };
 
